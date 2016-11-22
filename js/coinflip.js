456dd32abc01cf4b20e435e0d86255ef155e85af
vid Strickland <vidkid7@gmail.com>
2016-11-22 02:29:47 -0500

    Basic Functionality
    Added placeholder graphics, basic coin flip logic

--- js/coinflip.js
+++ js/coinflip.js
@@ -1,1 +1,32 @@
-console.log("connected!");
+coinPos = "heads";
+flipVal = 0;
+
+function coinFlip(){
+
+	flipVal = Math.random();
+
+	if((flipVal > .5) && coinPos == "heads"){
+		console.log("TEST INFO - COINPOS: " + coinPos + " | FLIPVAL: " + flipVal);
+		console.log("Flipped from Heads to Heads!");
+		coinPos = "heads";
+		document.getElementById("theCoin").src = "images/coinHtoH.gif";
+	}else if((flipVal <= .5) && coinPos == "heads"){
+		console.log("TEST INFO - COINPOS: " + coinPos + " | FLIPVAL: " + flipVal);
+		console.log("Flipped from Heads to Tails!");
+		coinPos = "tails";
+		document.getElementById("theCoin").src = "images/coinHtoT.gif";
+	}else if((flipVal > .5) && coinPos == "tails"){
+		console.log("TEST INFO - COINPOS: " + coinPos + " | FLIPVAL: " + flipVal);
+		console.log("Flipped from Tails to Heads!");
+		coinPos = "heads";
+		document.getElementById("theCoin").src = "images/coinTtoH.gif";
+	}else if((flipVal <= .5) && coinPos == "tails"){
+		console.log("TEST INFO - COINPOS: " + coinPos + " | FLIPVAL: " + flipVal);
+		console.log("Flipped from Tails to Tails!");
+		coinPos = "tails";
+		document.getElementById("theCoin").src = "images/coinTtoT.gif";
+	}else{
+		console.log("TEST INFO - COINPOS: " + coinPos + " | FLIPVAL: " + flipVal);
+		console.log("Something unexpected happened!");
+	}
+}
